1b2b209e0fb81f16babd693b52169db939bd40f6
LMajcherWsei <100759817+LMajcherWsei@users.noreply.github.com>
2023-12-11 14:56:41 +0100

    first commit

---
+++ src/main/java/com/lukasz_majcher/web_api_client/WebApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.lukasz_majcher.web_api_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukasz_majcher/web_api_client/WebApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukasz_majcher.web_api_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
